e282fa37be6cf19460bbc471c1459b9b370dff5e
AJ  Twiss <ajtwiss@peachjar.com>
2017-02-09 21:51:37 -0800

    adding pacages for webpack

---
+++ entry.js
@@ -0,0 +1,10 @@
+/*
+* @Author: AJ  Twiss
+* @Date:   2017-02-09 21:42:58
+* @Last Modified by:   AJ  Twiss
+* @Last Modified time: 2017-02-09 21:44:14
+*/
+
+'use strict';
+
+console.log("It works.");
