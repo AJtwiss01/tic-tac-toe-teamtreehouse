06ca41a076084b6805321ce74c40861738c6bceb
Arthur J Twiss <arthurjtwiss@Arthurs-MacBook-Pro.local>
2017-02-12 21:29:05 -0800

    added svg to mousenter event

--- bundle.js
+++ bundle.js
@@ -376,7 +376,18 @@ function updateLink(linkElement, obj) {
 
 /***/ }),
 /* 2 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/*
+* @Author: AJ  Twiss
+* @Date:   2017-02-09 23:26:29
+* @Last Modified by:   AJ  Twiss
+* @Last Modified time: 2017-02-09 23:36:34
+*/
+
+
+
 
  var board = document.getElementById('board');
  var start = document.getElementById('start');
@@ -412,7 +423,7 @@ for (var i = 0; i < box.length; i++) {
 /* 3 */
 /***/ (function(module, exports) {
 
-module.exports = "<!DOCTYPE html>\n<html lang=\"en\">\n\n<head>\n    <meta charset=\"UTF-8\">\n    <title>Tic Tac Toe</title>\n    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>\n</head>\n\n<body>\n    <div class=\"screen screen-start\" id=\"start\">\n        <header>\n            <h1>Tic Tac Toe</h1>\n            <a href=\"#\" class=\"button\">Start game</a>\n        </header>\n    </div>\n    <div class=\"board\" id=\"board\">\n        <header>\n            <h1>Tic Tac Toe</h1>\n            <ul>\n                <li class=\"players\" id=\"player1\">\n                    <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"42\" height=\"42\" viewBox=\"0 0 42 42\" version=\"1.1\">\n                        <g stroke=\"none\" stroke-width=\"1\" fill=\"none\" fill-rule=\"evenodd\">\n                            <g transform=\"translate(-200.000000, -60.000000)\" fill=\"#000000\">\n                                <g transform=\"translate(200.000000, 60.000000)\">\n                                    <path d=\"M21 36.6L21 36.6C29.6 36.6 36.6 29.6 36.6 21 36.6 12.4 29.6 5.4 21 5.4 12.4 5.4 5.4 12.4 5.4 21 5.4 29.6 12.4 36.6 21 36.6L21 36.6ZM21 42L21 42C9.4 42 0 32.6 0 21 0 9.4 9.4 0 21 0 32.6 0 42 9.4 42 21 42 32.6 32.6 42 21 42L21 42Z\" />\n                                </g>\n                            </g>\n                        </g>\n                    </svg>\n                </li>\n                <li class=\"players\" id=\"player2\">\n                    <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"42\" height=\"43\" viewBox=\"0 0 42 43\" version=\"1.1\">\n                        <g stroke=\"none\" stroke-width=\"1\" fill=\"none\" fill-rule=\"evenodd\">\n                            <g transform=\"translate(-718.000000, -60.000000)\" fill=\"#000000\">\n                                <g transform=\"translate(739.500000, 81.500000) rotate(-45.000000) translate(-739.500000, -81.500000) translate(712.000000, 54.000000)\">\n                                    <path d=\"M30 30.1L30 52.5C30 53.6 29.1 54.5 28 54.5L25.5 54.5C24.4 54.5 23.5 53.6 23.5 52.5L23.5 30.1 2 30.1C0.9 30.1 0 29.2 0 28.1L0 25.6C0 24.5 0.9 23.6 2 23.6L23.5 23.6 23.5 2.1C23.5 1 24.4 0.1 25.5 0.1L28 0.1C29.1 0.1 30 1 30 2.1L30 23.6 52.4 23.6C53.5 23.6 54.4 24.5 54.4 25.6L54.4 28.1C54.4 29.2 53.5 30.1 52.4 30.1L30 30.1Z\" />\n                                </g>\n                            </g>\n                        </g>\n                    </svg>\n                </li>\n            </ul>\n        </header>\n        <ul class=\"boxes\">\n            <li class=\"box\"></li>\n            <li class=\"box\"></li>\n            <li class=\"box\"></li>\n            <li class=\"box\"></li>\n            <li class=\"box\"></li>\n            <li class=\"box\"></li>\n            <li class=\"box\"></li>\n            <li class=\"box\"></li>\n            <li class=\"box\"></li>\n        </ul>\n    </div>\n\n            <script type=\"text/javascript\" src=\"bundle.js\" charset=\"utf-8\"></script>\n\n</body>\n\n</html>\n";
+module.exports = "<!DOCTYPE html>\n<html lang=\"en\">\n\n<head>\n    <meta charset=\"UTF-8\">\n    <title>Tic Tac Toe</title>\n    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>\n</head>\n\n<body>\n    <div class=\"screen screen-start\" id=\"start\">\n        <header>\n            <h1>Tic Tac Toe</h1>\n            <a href=\"#\" class=\"button\">Start game</a>\n        </header>\n    </div>\n    <div class=\"board\" id=\"board\">\n        <header>\n            <h1>Tic Tac Toe</h1>\n            <ul>\n                <li class=\"players\" id=\"player1\">\n                    <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"42\" height=\"42\" viewBox=\"0 0 42 42\" version=\"1.1\">\n                        <g stroke=\"none\" stroke-width=\"1\" fill=\"none\" fill-rule=\"evenodd\">\n                            <g transform=\"translate(-200.000000, -60.000000)\" fill=\"#000000\">\n                                <g transform=\"translate(200.000000, 60.000000)\">\n                                    <path d=\"M21 36.6L21 36.6C29.6 36.6 36.6 29.6 36.6 21 36.6 12.4 29.6 5.4 21 5.4 12.4 5.4 5.4 12.4 5.4 21 5.4 29.6 12.4 36.6 21 36.6L21 36.6ZM21 42L21 42C9.4 42 0 32.6 0 21 0 9.4 9.4 0 21 0 32.6 0 42 9.4 42 21 42 32.6 32.6 42 21 42L21 42Z\" />\n                                </g>\n                            </g>\n                        </g>\n                    </svg>\n                </li>\n                <li class=\"players\" id=\"player2\">\n                    <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"42\" height=\"43\" viewBox=\"0 0 42 43\" version=\"1.1\">\n                        <g stroke=\"none\" stroke-width=\"1\" fill=\"none\" fill-rule=\"evenodd\">\n                            <g transform=\"translate(-718.000000, -60.000000)\" fill=\"#000000\">\n                                <g transform=\"translate(739.500000, 81.500000) rotate(-45.000000) translate(-739.500000, -81.500000) translate(712.000000, 54.000000)\">\n                                    <path d=\"M30 30.1L30 52.5C30 53.6 29.1 54.5 28 54.5L25.5 54.5C24.4 54.5 23.5 53.6 23.5 52.5L23.5 30.1 2 30.1C0.9 30.1 0 29.2 0 28.1L0 25.6C0 24.5 0.9 23.6 2 23.6L23.5 23.6 23.5 2.1C23.5 1 24.4 0.1 25.5 0.1L28 0.1C29.1 0.1 30 1 30 2.1L30 23.6 52.4 23.6C53.5 23.6 54.4 24.5 54.4 25.6L54.4 28.1C54.4 29.2 53.5 30.1 52.4 30.1L30 30.1Z\" />\n                                </g>\n                            </g>\n                        </g>\n                    </svg>\n                </li>\n            </ul>\n        </header>\n        <ul class=\"boxes\">\n            <li class=\"box\"></li>\n            <li class=\"box\"></li>\n            <li class=\"box\"></li>\n            <li class=\"box\"></li>\n            <li class=\"box\"></li>\n            <li class=\"box\"></li>\n            <li class=\"box\"></li>\n            <li class=\"box\"></li>\n            <li class=\"box\"></li>\n        </ul>\n    </div>\n            <script src=\"../bundle.js\"></script>\n\n</body>\n\n</html>\n";
 
 /***/ }),
 /* 4 */
@@ -515,7 +526,7 @@ exports.push([module.i, "@charset \"UTF-8\";\nbody {\n  background: #fff;\n  fon
 * @Author: AJ  Twiss
 * @Date:   2017-02-09 21:42:58
 * @Last Modified by:   AJ  Twiss
-* @Last Modified time: 2017-02-09 23:10:18
+* @Last Modified time: 2017-02-09 23:36:16
 */
 
 
@@ -524,7 +535,7 @@ __webpack_require__(4);
 __webpack_require__(5);
 __webpack_require__(6);
 __webpack_require__(7);
-document.write(__webpack_require__(2));
+__webpack_require__(2);
 
 
 /***/ })
